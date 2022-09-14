9abbbc45bbb578a7c916a3ed4ada45c02451704f
Naitais <enzo.f9849@gmail.com>
2022-09-14 00:17:09 -0300

    changed a lot in the code and used an object for the calculator

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,311 @@
-let anyButton= document.getElementsByTagName("button");
+class Calculator{
+    constructor(currentOperandText, previousOperandText){
+        this.currentOperandText=currentOperandText;
+        this.previousOperandText=previousOperandText;
+        this.clearConsole()
+    }
+    clearConsole(){
+        this.currentOperand=""
+        this.previousOperand=""
+        this.operation=undefined
+    }
+    delete(){
+        this.currentOperand=this.currentOperand.toString().slice(0, -1)
+    }
+    getMemeSound = ()=>{
+        const sounds=[lautiSound, theRock, memeFour, memeThree, memeFive, memeSix,
+        memeSeven, memeEight, memeNine, memeTen, memeEleven, memeTwelve]
+        let sound = sounds[Math.floor(Math.random()*sounds.length)];
+        return sound;
+        }
+    getNumber(number){
+        if(number==="." && this.currentOperand.includes(".")) return
+        this.currentOperand=this.currentOperand.toString()+number.toString()
+    }
+
+    getOperation(operation){
+        if(this.currentOperand==="") return
+        if(this.currentOperand!==""){
+            this.compute()
+        }
+        this.operation=operation
+        this.previousOperand=this.currentOperand+" "+operation
+        this.currentOperand =""
+    }
+
+    compute(){
+        let computation;
+        const prev =parseFloat(this.previousOperand)
+        const current =parseFloat(this.currentOperand)
+        if(this.currentOperand==="") return
+        switch(this.operation){
+            case "+":
+                computation=prev+current
+                break
+            case "-":
+                computation=prev-current
+                break
+            case "/":
+                computation=prev/current
+                break
+            case "*":
+                computation=prev*current
+                break
+            default:
+                return
+        }
+        if (computation%1!==0)this.currentOperand=computation.toFixed(2)
+        else this.currentOperand=computation
+        this.operation=undefined
+        this.previousOperand=""
+    }
+
+    updateDisplay(){
+        this.currentOperandText.innerText = this.currentOperand
+        this.previousOperandText.innerText = this.previousOperand
+    }
+
+    touchEndAnimation(buttonType){
+        buttonType.forEach(button=>{
+            button.addEventListener("touchend",(e)=>{
+                //adds button animation
+                e.target.classList.remove("clickedButton");
+                e.target.classList.add("idleButton");
+
+                if(e.target.id === "undoButton"){
+                    undoButton.style.borderRight="6px solid black";
+                    undoButton.style.borderBottom="6px solid black";
+    
+                }else if(e.target.id === "clearButton"){
+                    
+                    clearButton.style.borderRight="6px solid rgb(120, 119, 119)";
+                    clearButton.style.borderBottom="6px solid rgb(120, 119, 119)";
+    
+                }else if(e.target.id === "memeButton"){
+                    
+                    memeButton.style.borderRight="6px solid rgb(60, 50, 69, .6)";
+                    memeButton.style.borderBottom="6px solid rgb(60, 50, 69, .6)";
+                }
+            })
+        })
+    }
+}
+
+const numberButtons=document.querySelectorAll("[data-number]");
+const operators=document.querySelectorAll("[data-operator]");
+const undoButton=document.querySelector("[data-undoButton]");
+const clearButton=document.querySelector("[data-clearButton]");
+const memeButton=document.querySelector("[data-memeButton]");
+const equalButton=document.querySelector("[data-equal]");
+const previousOperandText=document.querySelector("[data-previous-operand");
+const currentOperandText=document.querySelector("[data-current-operand]");
+const anyButton=document.getElementsByTagName("button");
+
+
+const calculator=new Calculator(currentOperandText, previousOperandText)
+
+numberButtons.forEach(button=>{
+    button.addEventListener("touchstart",(e)=>{
+        calculator.getNumber(button.innerText)
+        calculator.updateDisplay()
+
+        calculator.touchEndAnimation(numberButtons)
+    })
+})
+
+operators.forEach(button=>{
+    button.addEventListener("touchstart",(e)=>{
+        calculator.getOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+clearButton.addEventListener("touchstart", ()=>{
+    calculator.clearConsole()
+    calculator.updateDisplay()
+})
+
+equalButton.addEventListener("touchstart",()=>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+undoButton.addEventListener("touchstart", ()=>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+
+for(let i=0;i<anyButton.length;i++){
+    anyButton[i].addEventListener("touchstart", (e)=>{
+        
+        e.target.classList.remove("idleButton");
+        e.target.classList.add("clickedButton");
+
+        if(e.target.id==="clearButton"){
+            clearButton.style.borderBottom="none";
+            clearButton.style.borderRight="none";
+
+        }else if(e.target.id==="undoButton"){
+            undoButton.style.borderBottom="none";
+            undoButton.style.borderRight="none";
+
+
+        }else if(e.target.id==="memeButton"){
+            memeButton.style.borderBottom="none";
+            memeButton.style.borderRight="none";
+            calculator.getMemeSound().currentTime=0;
+            calculator.getMemeSound().play()
+        }   
+        
+    });
+    anyButton[i].addEventListener("touchend", (e)=>{
+        
+        e.target.classList.remove("clickedButton");
+        e.target.classList.add("idleButton");
+
+        if(e.target.id === "undoButton"){
+            undoButton.style.borderRight="6px solid black";
+            undoButton.style.borderBottom="6px solid black";
+
+        }else if(e.target.id === "clearButton"){
+            
+            clearButton.style.borderRight="6px solid rgb(120, 119, 119)";
+            clearButton.style.borderBottom="6px solid rgb(120, 119, 119)";
+
+        }else if(e.target.id === "memeButton"){
+            
+            memeButton.style.borderRight="6px solid rgb(60, 50, 69, .6)";
+            memeButton.style.borderBottom="6px solid rgb(60, 50, 69, .6)";
+        }
+        
+    });
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let anyButton= document.getElementsByTagName("button");
 let undoButton=document.getElementById("undoButton");
 let clearButton=document.getElementById("clearButton");
 let memeButton=document.getElementById("memeButton");
 let firstRowButtons=document.getElementById("firstRowButtons");
 let zeroButton=document.getElementById("zeroButton");
 let displayCon=document.getElementById("displayCon");
+const numberList=document.createElement("ul");
+displayCon.append(numberList);*/
 
-const getMemeSound = ()=>{
+/*
+class Calculator{
+    constructor(numbers, result){
+        this.numbers=numbers;
+        this.result=result;
+    }
+    getMemeSound = ()=>{
     const sounds=[lautiSound, theRock, memeFour, memeThree, memeFive, memeSix,
     memeSeven, memeEight, memeNine, memeTen, memeEleven, memeTwelve]
     let sound = sounds[Math.floor(Math.random()*sounds.length)];
     return sound;
+    }
+    clearConsole = ()=>{
+        while(numberList.firstChild){
+            numberList.removeChild(numberList.firstChild);
+        }
+    }
+    getUserInput=()=>{
+    
+        let number;
+        
+        let symbol;
+        for(let i=0;i<anyButton.length;i++){
+            anyButton[i].addEventListener("touchstart", (e)=>{
+                
+                e.target.classList.remove("idleButton");
+                e.target.classList.add("clickedButton");
+    
+                }else if(e.target.id==="clearButton"){
+                    clearButton.style.borderBottom="none";
+                    clearButton.style.borderRight="none";
+                    clearConsole()
+    
+                }else if(e.target.id==="undoButton"){
+                    undoButton.style.borderBottom="none";
+                    undoButton.style.borderRight="none";
+                    numberList.removeChild(numberList.lastChild)
+    
+                }else if(e.target.id==="memeButton"){
+                    memeButton.style.borderBottom="none";
+                    memeButton.style.borderRight="none";
+                    getMemeSound().currentTime=0;
+                    getMemeSound().play()
+                }else if(e.target.id==="equal"){
+                    
+                    getB(numbers)
+                    numbers.length=0;
+                    sum(result)
+                   
+                    
+                }     
+                
+            });
+            anyButton[i].addEventListener("touchend", (e)=>{
+                
+                e.target.classList.remove("clickedButton");
+                e.target.classList.add("idleButton");
+    
+                if(e.target.id === "undoButton"){
+                    undoButton.style.borderRight="6px solid black";
+                    undoButton.style.borderBottom="6px solid black";
+    
+                }else if(e.target.id === "clearButton"){
+                    
+                    clearButton.style.borderRight="6px solid rgb(120, 119, 119)";
+                    clearButton.style.borderBottom="6px solid rgb(120, 119, 119)";
+    
+                }else if(e.target.id === "memeButton"){
+                    
+                    memeButton.style.borderRight="6px solid rgb(60, 50, 69, .6)";
+                    memeButton.style.borderBottom="6px solid rgb(60, 50, 69, .6)";
+                }
+                
+            });
+             
+        }
+        
+        
+    }
+
 }
 
-/**/
+
+
+
 let numbers=[];
 let operator;
 let result=[];
-let a=[];
-let b=[];
+
 const operation=(operator)=>{
 }
 const sum=()=>{
@@ -47,200 +334,14 @@ getB=(array)=>{
                    
 }
 
-const numberList=document.createElement("ul");
-displayCon.append(numberList);
 
-const clearConsole = ()=>{
-    while(numberList.firstChild){
-        numberList.removeChild(numberList.firstChild);
-    }
-}
 
-const getUserInput=()=>{
-    
-    let number;
-    
-    let symbol;
-    for(let i=0;i<anyButton.length;i++){
-        anyButton[i].addEventListener("touchstart", (e)=>{
-            
-            e.target.classList.remove("idleButton");
-            e.target.classList.add("clickedButton");
-            
-            if(e.target.id==="zeroButton"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=0;
-                number=0;
-                numbers.push(number);
-                console.log(numbers);
-                return number;
 
-            }else if(e.target.id==="one"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=1;
-                number=1;
-                numbers.push(number);
-                return number;
-                
-            }else if(e.target.id==="two"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=2;
-                number=2;
-                numbers.push(number);
-                return number;
-            
-            } else if(e.target.id==="three"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=3;
-                number=3;
-                numbers.push(number);
-                return number;
-        
-            }else if(e.target.id==="four"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=4;
-                number=4;
-                numbers.push(number);
-                return number;
-            
-            }else if(e.target.id==="five"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=5;
-                number=5;
-                numbers.push(number);
-                return number;
-            
-            }else if(e.target.id==="six"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=6;
-                number=6;
-                numbers.push(number);
-                return number;
-            
-            }else if(e.target.id==="seven"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=7;
-                number=7;
-                numbers.push(number);
-                return number;
-            
-            }else if(e.target.id==="eight"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=8;
-                number=8;
-                numbers.push(number);
-                return number;
-            
-            }else if(e.target.id==="nine"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=9;
-                number=9;
-                numbers.push(number);
-                return number;
 
-            }else if(e.target.id==="mult"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent="*";
-                operator="multiplication";
-                getA(numbers)
-                return operator;
 
-            }else if(e.target.id==="divide"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent="/";
-                operator="division";
-                getA(numbers)
-                return operator;
-
-            }else if(e.target.id==="minus"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent="-";
-                operator="reduction";
-                getA(numbers)
-                return operator;
-
-            }else if(e.target.id==="sum"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent="+";
-                operator="sum";
-                getA(numbers)
-                numbers.length=0;
-                return operator;
-
-            }else if(e.target.id==="dot"){
-                let newNumber=document.createElement("li");
-                numberList.append(newNumber);
-                newNumber.textContent=".";
-                symbol=".";
-                return symbol;
-
-            }else if(e.target.id==="clearButton"){
-                clearButton.style.borderBottom="none";
-                clearButton.style.borderRight="none";
-                clearConsole()
-
-            }else if(e.target.id==="undoButton"){
-                undoButton.style.borderBottom="none";
-                undoButton.style.borderRight="none";
-                numberList.removeChild(numberList.lastChild)
-
-            }else if(e.target.id==="memeButton"){
-                memeButton.style.borderBottom="none";
-                memeButton.style.borderRight="none";
-                getMemeSound().currentTime=0;
-                getMemeSound().play()
-            }else if(e.target.id==="equal"){
-                
-                getB(numbers)
-                numbers.length=0;
-                sum(result)
-               
-                
-            }     
-            
-        });
-        anyButton[i].addEventListener("touchend", (e)=>{
-            
-            e.target.classList.remove("clickedButton");
-            e.target.classList.add("idleButton");
-
-            if(e.target.id === "undoButton"){
-                undoButton.style.borderRight="6px solid black";
-                undoButton.style.borderBottom="6px solid black";
-
-            }else if(e.target.id === "clearButton"){
-                
-                clearButton.style.borderRight="6px solid rgb(120, 119, 119)";
-                clearButton.style.borderBottom="6px solid rgb(120, 119, 119)";
-
-            }else if(e.target.id === "memeButton"){
-                
-                memeButton.style.borderRight="6px solid rgb(60, 50, 69, .6)";
-                memeButton.style.borderBottom="6px solid rgb(60, 50, 69, .6)";
-            }
-            
-        });
-         
-    }
-    
-    
-}
 
 
 operation()
 getUserInput()
 
+*/
